c056f17b918f2c4074730762a7c350762fe630b4
dee <dpulaak@gmail.com>
2018-12-01 00:51:37 +0600

    Updated readme, added cal.py

---
+++ cal.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+import turtle
+
+turtle.shape("turtle")
+
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.lef(90)
+turtle.forward(100)
+turtle.left(90)
+
+turtle.exitonclic()
+
+
